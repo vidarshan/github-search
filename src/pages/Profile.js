2bf0ae11749549f64d6472e69c6ac5dd87f6b79c
Vidarshan <vidarshanadithya3@gmail.com>
2021-10-12 19:18:06 +0530

    Refactored profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -16,7 +16,7 @@ const Profile = ({ match }) => {
     const history = useHistory();
     const dispatch = useDispatch();
 
-    const { loading: userLoading, error: errorLoading, getUser: user } = useSelector((state) => state.getUser);
+    const { loading: userLoading, error: userError, getUser: user } = useSelector((state) => state.getUser);
     const { loading: repoLoading, error: repoError, userRepos: repos } = useSelector((state) => state.userRepos);
     const { loading: starredLoading, error: starredError, userStarred: starred } = useSelector((state) => state.userStarred);
 
@@ -28,130 +28,131 @@ const Profile = ({ match }) => {
         dispatch(getUserInfo(match.params.name))
         dispatch(getUserRepos(match.params.name))
         dispatch(getUserStarred(match.params.name))
-    }, [dispatch])
+    }, [dispatch, match])
 
     return (
+        <Fade direction={'left'}>
 
-        <div className='profile'>
-            <div class="back-btn" onClick={() => goBackHandler()}>
-                <div class="icon">
-                    <FiArrowLeft />
-                </div>
-                <div class="text">
-                    Back to Results
-                </div>
-            </div>
-            {/* <span className='user-type'>{user.type === 'User' ? '👤' : '🛡️'}</span> */}
-            <div class="img-container">
-                <img className='profile-img' src={user.avatar_url} alt="" />
-                <span className='profile-name'>{user.name}</span>
-                <span className='profile-bio'>{user.bio}</span>
-            </div>
-            <div class="follow-social">
-                <div class="following-followers">
-                    <div className="follows">
-                        <span className='followers-number'><CountUp duration={1} end={user.followers} separator=',' /></span>
-                        <span className="text">Followers</span>
+            <div className='profile'>
+                <div className="back-btn" onClick={() => goBackHandler()}>
+                    <div className="icon">
+                        <FiArrowLeft />
                     </div>
-                    <div className="follows">
-
-                        <span className='following-number'><CountUp duration={5} end={user.following} /></span>
-                        <span className="text">Following</span>
+                    <div className="text">
+                        Back to Results
                     </div>
                 </div>
-                <div class="blog-twitter-email">
-                    <div class="social">
-                        🌎 Website
-                        <br />
-                        <span>{user.blog}</span>
-                    </div>
-                    <div class="social">
-                        📩 Twitter
-                        <br />
-                        <span>{user.twitter_username}</span>
-                    </div>
+                {/* <span className='user-type'>{user.type === 'User' ? '👤' : '🛡️'}</span> */}
+                <div className="img-container">
+                    <img className='profile-img' src={user.avatar_url} alt="" />
+                    <span className='profile-name'>{user.name}</span>
+                    <span className='profile-bio'>{user.bio}</span>
                 </div>
-            </div>
-
-            <br />
-            <div class="info">
-                <div class="section-heading"><VscOctoface /> Profile</div>
-                <div class="info-grid">
-                    <div class="item">
-                        📍 Location
-                        <br />
-                        <span>{user.location}</span>
-                    </div>
-                    <div class="item">
-                        🏢 Organizations
-                        <br />
-                        <span>{user.company}</span>
-                    </div>
-                    <div class="item">
-                        📅 Date Joined
-                        <br />
-                        <span>{moment(user.created_at).format('MM-DD-YYYY')}</span>
-                    </div>
-                    <div class="item">
-                        📅 Last Updated
-                        <br />
-                        <span>{moment(user.updated_at).format('MM-DD-YYYY')}</span>
-                    </div>
-                    <div class="item">
-                        💵 Hireable?
-                        <br />
-                        <span>{user.hireable ? 'Yes' : 'No'}</span>
-                    </div>
-                    <div class="item">
-                        👨‍💻 Site Admin?
-                        <br />
-                        <span>{user.site_admin ? 'Yes' : 'No'}</span>
-                    </div>
-                    <div class="item">
-                        Public Gists
-                        <br />
-                        <span>{user.public_gists}</span>
-                    </div>
-                    <div class="item">
-                        Public Repos
-                        <br />
-                        <span>{user.public_repos}</span>
-                    </div>
-                    <div class="item">
-                        Starred
-                        <br />
-                        <span>{starred.length}</span>
-                    </div>
-                </div>
-            </div>
-
-            <div class="items">
-                <div class="section-heading"><VscFolder /> Public Repositories</div>
-                <div class="items-grid">
-
-                    {map(repos, (repo) => {
-                        return <a className='items' href={replace(repo.url, 'api.github.com/repos', 'github.com')} target='_blank'>
-                            <div class="icon"><VscFolder /></div>
-                            <div class="name">{repo.name}</div>
-                        </a>
-                    })}
-
-                </div>
-            </div>
-
-            <div class="items">
-                <div class="section-heading"><VscStarFull /> Starred Repositories</div>
-                {console.log(starred)}
-                <div class="items-grid">
-                    {map(starred, (star) => {
-                        return <div className='items' href={replace(star.url, 'api.github.com/repos', 'github.com')}>
-                            <div class="icon"><VscStarFull /></div>
-                            <div class="name">{star.name}</div>
+                <div className="follow-social">
+                    <div className="following-followers">
+                        <div className="follows">
+                            <span className='followers-number'><CountUp duration={1} end={user.followers} separator=',' /></span>
+                            <span className="text">Followers</span>
                         </div>
-                    })}
+                        <div className="follows">
+
+                            <span className='following-number'><CountUp duration={5} end={user.following} /></span>
+                            <span className="text">Following</span>
+                        </div>
+                    </div>
+                    <div className="blog-twitter-email">
+                        <div className="social">
+                            🌎 Website
+                            <br />
+                            <span>{user.blog}</span>
+                        </div>
+                        <div className="social">
+                            📩 Twitter
+                            <br />
+                            <span>{user.twitter_username}</span>
+                        </div>
+                    </div>
+                </div>
+
+                <br />
+                <div className="info">
+                    <div className="section-heading"><VscOctoface /> Profile</div>
+                    <div className="info-grid">
+                        <div className="item">
+                            📍 Location
+                            <br />
+                            <span>{user.location}</span>
+                        </div>
+                        <div className="item">
+                            🏢 Organizations
+                            <br />
+                            <span>{user.company}</span>
+                        </div>
+                        <div className="item">
+                            📅 Date Joined
+                            <br />
+                            <span>{moment(user.created_at).format('MM-DD-YYYY')}</span>
+                        </div>
+                        <div className="item">
+                            📅 Last Updated
+                            <br />
+                            <span>{moment(user.updated_at).format('MM-DD-YYYY')}</span>
+                        </div>
+                        <div className="item">
+                            💵 Hireable?
+                            <br />
+                            <span>{user.hireable ? 'Yes' : 'No'}</span>
+                        </div>
+                        <div className="item">
+                            👨‍💻 Site Admin?
+                            <br />
+                            <span>{user.site_admin ? 'Yes' : 'No'}</span>
+                        </div>
+                        <div className="item">
+                            Public Gists
+                            <br />
+                            <span>{user.public_gists}</span>
+                        </div>
+                        <div className="item">
+                            Public Repos
+                            <br />
+                            <span>{user.public_repos}</span>
+                        </div>
+                        <div className="item">
+                            Starred
+                            <br />
+                            <span>{starred.length}</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="items">
+                    <div className="section-heading"><VscFolder /> Public Repositories</div>
+                    <div className="items-grid">
+
+                        {map(repos, (repo) => {
+                            return <a className='items' rel="noreferrer" href={replace(repo.url, 'api.github.com/repos', 'github.com')} target='_blank'>
+                                <div className="icon"><VscFolder /></div>
+                                <div className="name">{repo.name}</div>
+                            </a>
+                        })}
+
+                    </div>
+                </div>
+
+                <div className="items">
+                    <div className="section-heading"><VscStarFull /> Starred Repositories</div>
+                    <div className="items-grid">
+                        {map(starred, (star) => {
+                            return <div className='items' rel="noreferrer" href={replace(star.url, 'api.github.com/repos', 'github.com')}>
+                                <div className="icon"><VscStarFull /></div>
+                                <div className="name">{star.name}</div>
+                            </div>
+                        })}
+                    </div>
                 </div>
             </div>
-        </div>
+        </Fade>
 
     )
 }
