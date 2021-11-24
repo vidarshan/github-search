f07cf1634b1ea515e96bb303532e0f88bbb06837
Vidarshan <vidarshanadithya3@gmail.com>
2021-11-24 19:33:42 +0530

    Added antdesign

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Footer from './components/Footer';
-import Profile from './pages/Profile';
-import Results from './pages/Results';
-import './styles/styles.scss';
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import Profile from "./pages/Profile";
+import Results from "./pages/Results";
+import "antd/dist/antd.css";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route path='/' component={Home} exact />
-        <Route path='/search/:word' component={Results} />
-        <Route path='/profile/:name' component={Profile} />
+        <Route path="/" component={Home} exact />
+        <Route path="/search/:word" component={Results} />
+        <Route path="/profile/:name" component={Profile} />
       </div>
       <Footer />
     </Router>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,40 +1,67 @@
-import { useState } from 'react';
-import { Fade } from 'react-awesome-reveal';
+import { Row, Col, Input, Space } from "antd";
+import { AudioOutlined } from "@ant-design/icons";
+
+import { useState } from "react";
+
+import { Fade } from "react-awesome-reveal";
 import { FaSearch } from "react-icons/fa";
-import { BsGithub } from 'react-icons/bs';
-import { useHistory } from 'react-router-dom';
+import { BsGithub } from "react-icons/bs";
+import { useHistory } from "react-router-dom";
 
-
+const { Search } = Input;
 
 const Home = () => {
+  const history = useHistory();
 
-    const history = useHistory();
+  const [keyword, setKeyword] = useState();
+  const [disable, setDisable] = useState(true);
 
-    const [keyword, setKeyword] = useState();
-    const [disable, setDisable] = useState(true);
-
-    const searchHandler = (word) => {
-        if (!word) {
-            setDisable(true)
-        } else {
-            setKeyword(word)
-            setDisable(false)
-        }
+  const searchHandler = (word) => {
+    if (!word) {
+      setDisable(true);
+    } else {
+      setKeyword(word);
+      setDisable(false);
     }
+  };
 
-    return (
-        <Fade direction={'left'}>
-            <div className='home'>
-                <div class="icon"><BsGithub /> </div>
-                <div class="heading">Githuber</div>
-                <div class="sub-heading">Github search made simple</div>
-                <div class="search">
-                    <input type="text" placeholder='Search for a Github User' onChange={(e) => searchHandler(e.target.value)} />
-                    <button disabled={disable} className='search-btn' onClick={() => history.push(`/search/${keyword}`)}><FaSearch /></button>
-                </div>
-            </div>
-        </Fade>
-    )
-}
+  return (
+    // <Fade direction={"left"}>
+    <Row>
+      <Col style={{ backgroundColor: "red" }} span={24}>
+        <Space>
+          <Search
+            size="large"
+            placeholder="input search text"
+            onSearch={searchHandler}
+            style={{ width: 500 }}
+          />
+        </Space>
+      </Col>
+    </Row>
+    // <div className="home">
+    //     <div class="icon">
+    //       <BsGithub />{" "}
+    //     </div>
+    //     <div class="heading">Githuber</div>
+    //     <div class="sub-heading">Github search made simple</div>
+    //     <div class="search">
+    //       <input
+    //         type="text"
+    //         placeholder="Search for a Github User"
+    //         onChange={(e) => searchHandler(e.target.value)}
+    //       />
+    //       <button
+    //         disabled={disable}
+    //         className="search-btn"
+    //         onClick={() => history.push(`/search/${keyword}`)}
+    //       >
+    //         <FaSearch />
+    //       </button>
+    //     </div>
+    //   </div>
+    // </Fade>
+  );
+};
 
-export default Home
+export default Home;
