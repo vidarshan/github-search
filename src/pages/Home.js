868f45b8de59a240fc3eca05b226904c764cd2f9
Vidarshan <vidarshanadithya3@gmail.com>
2022-02-18 19:00:56 +0530

    Version 3 initialization

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,16 @@ import { useHistory } from "react-router-dom";
 import { Row, Col, Input, Typography } from "antd";
 import { GithubOutlined } from "@ant-design/icons";
 import { Fade } from "react-awesome-reveal";
+import { AppShell, Burger, Container, Header, MediaQuery, Navbar, Text, TextInput, useMantineTheme } from '@mantine/core';
 
 const { Search } = Input;
 const { Title } = Typography;
 
 const Home = () => {
   const history = useHistory();
+
+  const [opened, setOpened] = useState(false);
+  const theme = useMantineTheme();
 
   const [keyword, setKeyword] = useState();
 
@@ -20,28 +24,35 @@ const Home = () => {
   };
 
   return (
-    <Fade direction={"left"}>
-      <div className="search-page">
-        <div className="search-container">
-          <Row justify="center">
-            <Title>
-              <GithubOutlined /> {"     "}Github Search
-            </Title>{" "}
-          </Row>
+    <Container sx={{ maxWidth: 'none', height: '100vh', backgroundColor: 'lightblue', display: "flex", justifyContent: 'center', alignItems: 'center' }}>
+      <TextInput
+        placeholder="Your name"
+        label="Full name"
+        required
+      />
+    </Container>
+    // <Fade direction={"left"}>
+    //   <div className="search-page">
+    //     <div className="search-container">
+    //       <Row justify="center">
+    //         <Title>
+    //           <GithubOutlined /> {"     "}Github Search
+    //         </Title>{" "}
+    //       </Row>
 
-          <Row justify="center">
-            <Col xl={10} lg={10} md={16} sm={20} xs={20}>
-              <Search
-                onChange={(e) => setKeyword(e.target.value)}
-                size="large"
-                placeholder="Search for a Github user"
-                onSearch={searchHandler}
-              />
-            </Col>
-          </Row>
-        </div>
-      </div>
-    </Fade>
+    //       <Row justify="center">
+    //         <Col xl={10} lg={10} md={16} sm={20} xs={20}>
+    //           {/* <Search
+    //             onChange={(e) => setKeyword(e.target.value)}
+    //             size="large"
+    //             placeholder="Search for a Github user"
+    //             onSearch={searchHandler}
+    //           /> */}
+    //         </Col>
+    //       </Row>
+    //     </div>
+    //   </div>
+    // </Fade>
   );
 };
 
