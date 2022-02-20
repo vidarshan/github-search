02533b79566f101da3029467172335bed63f6e6c
Vidarshan <vidarshanadithya3@gmail.com>
2022-02-20 14:46:11 +0530

    Repo card design completion

--- src/components/RepositoryCard.jsx
+++ src/components/RepositoryCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Card, Text, Group, Badge, Grid, Col, Container } from "@mantine/core";
 import { BsStarFill } from "react-icons/bs";
 import { BiGitBranch } from "react-icons/bi";
+import {GoPrimitiveDot} from 'react-icons/go';
 
 const RepositoryCard = ({
   name,
@@ -18,24 +19,27 @@ const RepositoryCard = ({
           <Text weight={600}>{name}</Text> 
         </Col>
         <Col span={6}>
-          <Text align='right' weight={600}>{language}</Text>
+        <div style={{display:'flex', alignItems:'center', justifyContent:'flex-end', width:'100%'}}>
+        <GoPrimitiveDot/>
+          <Text align='right' >{language}</Text>
+          </div>
         </Col>
         </Grid>
         <Grid>
         <Col sx={{margin:'1rem 0'}} span={12}>
           <Text size="sm" style={{ lineHeight: 1.5 }}>
-          {description}
+          {description.substring(0,100)+'...'}
         </Text>
         </Col>
         </Grid>
         <Group  direction='row' position='apart'>
           <div style={{display:'flex', alignItems:'center', justifyContent:'center'}}>
-          <BsStarFill style={{marginRight:'5px'}}/> 
-          <Text size='sm'> {starsCount}</Text>
+          <BsStarFill color='orange' style={{marginRight:'5px'}}/> 
+          <Text size='sm' weight={500}> {starsCount.toLocaleString()}</Text>
           </div>
           <div  direction='row' position='apart' style={{display:'flex', alignItems:'center', justifyContent:'center'}}>
-          <BiGitBranch style={{marginRight:'5px'}}/> 
-          <Text size='sm'> {forksCount.toLocaleString()}</Text>
+          <BiGitBranch color='blue' style={{marginRight:'5px'}}/> 
+          <Text size='sm' weight={500}> {forksCount.toLocaleString()}</Text>
           </div>
         </Group>
         {/* <Grid>
