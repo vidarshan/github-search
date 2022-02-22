baa6b103a67832d801df43d0c9415ea5eceeac85
Vidarshan <vidarshanadithya3@gmail.com>
2022-02-22 19:49:35 +0530

    Changes to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { Paper, Grid, Col, Group, Anchor, Text } from "@mantine/core";
 
 const Footer = () => {
   return (
-    <Paper sx={{ minHeight: '5vh', borderRadius: '0px', paddingBottom: '1rem' }}>
+    <Paper sx={{ minHeight: '5vh', borderRadius: '0px', paddingBottom: '1rem', marginTop: '5rem' }}>
       <Text size='xs' weight={700} align='center'>By Vidarshan</Text>
       <Text sx={{ marginTop: '.5rem' }} size='xs' weight={700} align='center'>Powered by Github API</Text>
     </Paper>
